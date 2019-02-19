320429031e7c0f3a6dc8257948da83e61fc18293
cesar <cmayorga@carecloud.com.co>
2019-02-19 10:09:21 -0500

    commit two
    commit two

--- app/models/inputandoutput.rb
+++ app/models/inputandoutput.rb
@@ -1,7 +1,10 @@
 class Inputandoutput < ApplicationRecord
-
+    # validate params
+    validates_presence_of :date
+    validates_presence_of :observation
+    validates_presence_of :user_id
+    # date 
     def obtain_date
         Date.today
-    end
-    
+    end  
 end
